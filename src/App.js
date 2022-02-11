742f1fa0f62fb878d6f1122735fd73f340f7594a
aditya-sudo <adityashah89156@gmail.com>
2022-02-11 12:40:27 +0530

    Feature:React-toolchain with npm,webpack and babel

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="center">
+      <h1>My React App</h1>
+      <br />
+      <h4 id="owner">By:Aditya Shah</h4>
+    </div>
+  );
+}
+
+export default App;
